730a9f5c015a4ad419459ae96150b689ff529f94
GvdDool <gijs.vandendool@gmail.com>
2026-02-16 20:00:03 +0100

    Local clean-up
    removed old bat files

--- start_db_streamlit.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo Starting IDP Housing Suitability Database...
-D:\GeoSpatial\anaconda3\envs\geo_env_20250322\python.exe -m streamlit run d:\GitHub\trainingGoogleTest\DataBaseManagementTool_HfH\streamlit_app.py
-pause
--- stop_db_streamlit.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Stopping all Python processes (Streamlit)...
-taskkill /IM python.exe /F
-echo.
-echo If you saw "SUCCESS", the application has been stopped.
-pause
